970b9221a22229874677f0a06022a8bac963fe22
promecarus <haikalslipi@gmail.com>
2023-09-20 09:57:23 +0700

    finish

--- app/src/main/java/com/promecarus/ticketingapps/MainActivity.kt
+++ app/src/main/java/com/promecarus/ticketingapps/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.promecarus.ticketingapps
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val seatView = findViewById<SeatsView>(R.id.seatsView)
+        val button = findViewById<Button>(R.id.finishButton)
+
+        button.setOnClickListener {
+            seatView.seat?.let {
+                Toast.makeText(this, "Kursi Anda nomor ${it.name}.", Toast.LENGTH_SHORT).show()
+            } ?: run {
+                Toast.makeText(this, "Silakan pilih kursi terlebih dahulu.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        }
     }
 }
